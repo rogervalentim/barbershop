3889b12a04f71247defe69a4ffe273de5e1d1506
rogervalentim <valentim.roger33@gmail.com>
2024-01-31 18:13:03 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
+import Header from "../_components/Header";
 
 export default function Home() {
   return (
-   <h1>fsw</h1>
+   <div>
+    <Header />
+   </div>
   );
 }
