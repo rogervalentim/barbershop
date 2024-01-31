e048f5f1660c039ae87a54ceded7b77c597ad634
rogervalentim <valentim.roger33@gmail.com>
2024-01-31 19:51:11 -0300

    feat: add initial Search component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,23 @@
-import Image from "next/image";
+import { format } from "date-fns";
 import Header from "../_components/Header";
+import { ptBR } from "date-fns/locale"
+import Search from "./_components/Search";
 
 export default function Home() {
   return (
    <div>
     <Header />
+
+    <h2>
+      Olá, Miguel!
+    </h2>
+    <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
+      locale: ptBR
+    })}</p>
+
+    <div className="px-5 mt-6">
+    <Search />
+    </div>
    </div>
   );
 }
