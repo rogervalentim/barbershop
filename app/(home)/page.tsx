e5b6c4c9c2a9eba87088fc59c6c0c443e6d30522
rogervalentim <valentim.roger33@gmail.com>
2024-01-31 22:25:25 -0300

    feat: add initial BarbershopItem

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,26 +3,42 @@ import Header from "../_components/header";
 import { ptBR } from "date-fns/locale"
 import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
+import { db } from "../_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item";
 
-export default function Home() {
+export default async function Home() {
+  const barbershops = await db.barbershop.findMany({})
+
   return (
    <div>
     <Header />
 
-    <h2>
-      Olá, Miguel!
-    </h2>
-    <p className="capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
-      locale: ptBR
-    })}</p>
+    <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
 
     <div className="px-5 mt-6">
     <Search />
     </div>
 
     <div className="px-5 mt-6">
-      <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
+    <h2 className="text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
       <BookingItem />
+    </div>
+
+    <div className="mt-6">
+    <h2 className="px-5 text-xs mb-3 uppercase text-gray-400 font-bold">Recomendados</h2>
+
+    <div className="flex gap-4 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+      {barbershops.map((barbershop) => (
+        <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+      ))}
+    </div>
     </div>
    </div>
   );
