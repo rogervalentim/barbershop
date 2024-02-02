f3e824a14167ac3a9bd608e8b01f6ed32f34c8a9
rogervalentim <valentim.roger33@gmail.com>
2024-02-02 15:55:34 -0300

    feat: add AuthProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "./_components/footer";
+import AuthProvider from "./_providers/auth";
 import "./globals.css";
 
 
@@ -19,7 +20,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} dark`}>
+        <AuthProvider>
         {children}
+        </AuthProvider>
         <Footer />
       </body>
     </html>
