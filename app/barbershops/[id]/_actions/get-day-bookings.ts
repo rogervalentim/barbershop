4c88815ffd28c48f2c412bc0201b6201e315b834
rogervalentim <valentim.roger33@gmail.com>
2024-02-03 22:03:45 -0300

    fix: get-day-bookings, add barbershopId

--- app/barbershops/[id]/_actions/get-day-bookings.ts
+++ app/barbershops/[id]/_actions/get-day-bookings.ts
@@ -1,15 +1,18 @@
-"use server"
+"use server";
 
 import { db } from "@/app/_lib/prisma";
 import { endOfDay, startOfDay } from "date-fns";
 
-export const getDayBookings = async (date: Date) => {
-    const bookings = await db.booking.findMany({
-        where: {
-            date: {
-                lte: endOfDay(date),
-                gte: startOfDay(date),
-            }
-        }
-    })
-}
+export const getDayBookings = async (barbershopId: string, date: Date) => {
+  const bookings = await db.booking.findMany({
+    where: {
+      barbershopId,
+      date: {
+        lte: endOfDay(date),
+        gte: startOfDay(date),
+      },
+    },
+  });
+
+  return bookings;
+};
