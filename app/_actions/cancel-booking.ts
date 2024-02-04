4b44a41a4f01e5ca23d875ab289752e5a20188dc
rogervalentim <valentim.roger33@gmail.com>
2024-02-04 20:51:34 -0300

    feat: ensure confirmation is shown on booking cancelation

--- app/_actions/cancel-booking.ts
+++ app/_actions/cancel-booking.ts
@@ -1,14 +1,15 @@
-"use server"
+"use server";
 
-import { revalidatePath } from "next/cache"
-import { db } from "../_lib/prisma"
+import { revalidatePath } from "next/cache";
+import { db } from "../_lib/prisma";
 
 export const cancelBooking = async (bookingId: string) => {
-    return await db.booking.delete({
-        where: {
-            id: bookingId,
-        }
-    })
+  await db.booking.delete({
+    where: {
+      id: bookingId,
+    },
+  });
 
-    revalidatePath("/bookings")
-}
+  revalidatePath("/");
+  revalidatePath("/bookings");
+};
