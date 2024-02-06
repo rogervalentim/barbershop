1a810c88a38d3c4de6a8745356750f7e666e9c0a
rogervalentim <valentim.roger33@gmail.com>
2024-02-06 16:31:36 -0300

    fix: rename files header and search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,12 @@
 import { format } from "date-fns";
 import { ptBR } from "date-fns/locale"
-import Search from "./_components/search";
 import BookingItem from "../_components/booking-item";
 import { db } from "../_lib/prisma"
 import BarbershopItem from "./_components/barbershop-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../_lib/auth";
 import Header from "../_components/header";
+import Search from "./_components/search";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
